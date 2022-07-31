54d8c7d10672791b752a7de937b1fc83d375a524
TheOctan <kanatush.tim@gmail.com>
2022-07-31 14:35:07 +0300

    Player raycasting

--- Assets/Project/Editor/CuttingControllerEditor.cs
+++ Assets/Project/Editor/CuttingControllerEditor.cs
@@ -7,24 +7,23 @@ public class CuttingControllerEditor : Editor
     private CuttingController _cuttingController;
 
     private SerializedProperty _castHeight;
+    private SerializedProperty _castDistance;
     private SerializedProperty _castRadius;
 
     private void OnEnable()
     {
         _cuttingController = target as CuttingController;
         _castHeight = serializedObject.FindProperty(nameof(_castHeight));
+        _castDistance = serializedObject.FindProperty(nameof(_castDistance));
         _castRadius = serializedObject.FindProperty(nameof(_castRadius));
     }
 
     private void OnSceneGUI()
     {
         Transform transform = _cuttingController.transform;
-        Vector3 position = transform.position;
-        Vector3 direction = transform.forward;
-        position.y = _castHeight.floatValue;
-        
+        Vector3 position = transform.position + Vector3.up * _castHeight.floatValue;
+
         Handles.color = Color.green;
-        Handles.DrawWireDisc(position, Vector3.up, _castRadius.floatValue);
-        Handles.DrawLine(position, position + direction * _castRadius.floatValue);
+        Handles.DrawWireDisc(position, Vector3.up, _castDistance.floatValue);
     }
 }
