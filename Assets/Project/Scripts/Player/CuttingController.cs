ed4661ed3244536b3915ed574c2b9f2fad9199d9
TheOctan <kanatush.tim@gmail.com>
2022-07-31 14:35:07 +0300

    Player raycasting

--- Assets/Project/Scripts/Player/CuttingController.cs
+++ Assets/Project/Scripts/Player/CuttingController.cs
@@ -1,14 +1,18 @@
 using System;
-using Project.Scripts.Field;
 using UnityEngine;
 
 public class CuttingController : MonoBehaviour
 {
-    [SerializeField] private float _castRadius = 1f;
+    [SerializeField] private float _castDistance = 1f;
+    [SerializeField] private float _castRadius = 0.3f;
     [SerializeField] private float _castHeight = 0.5f;
     [SerializeField] private LayerMask _layerMask;
 
+    [SerializeField] private GameObject _raycastedObject;
+    
+
     private IPlayerController _playerController;
+    private bool _isCast;
 
     private void Awake()
     {
@@ -30,18 +34,45 @@ public class CuttingController : MonoBehaviour
         Raycast();
     }
 
+    private void Update()
+    {
+        Raycast();
+    }
+
     private void Raycast()
     {
-        Vector3 position = transform.position;
-        position.y = _castHeight;
-
+        Vector3 position = transform.position + Vector3.up * _castHeight;
         var ray = new Ray(position, transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, _castRadius, _layerMask))
+
+        if (Physics.SphereCast(ray, _castRadius, out RaycastHit hit, _castDistance, _layerMask))
         {
             if (hit.collider.TryGetComponent(out IDestroyable obj))
             {
-                obj.Destroy();
+                // obj.Destroy();
+                _isCast = true;
+                _raycastedObject = hit.transform.gameObject;
+            }
+            else
+            {
+                _isCast = false;
+                _raycastedObject = null;
             }
         }
+        else
+        {
+            _isCast = false;
+            _raycastedObject = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 position = transform.position + Vector3.up * _castHeight;
+        Vector3 direction = transform.forward;
+
+        Gizmos.color = _isCast ? Color.red : Color.green;
+
+        Gizmos.DrawWireSphere(position + direction * _castDistance, _castRadius);
+        Gizmos.DrawRay(position, direction * _castDistance);
     }
 }
